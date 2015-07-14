4afc1098afe1611ffcd608978ed869766e27453c
Smriti Singh <smriti.singh@optimusinfo.com>
2015-07-14 12:53:40 +0530

    Completed the fifth assignment

---
+++ basic_syntax_and_constructs/Assignment5/Assignment5/TestArray.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    class TestArray
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(" ", getStringArray()));
+            Console.Read();
+        }
+
+
+        public static String[] getStringArray()
+        {
+            string[] array = new string[6];
+            array[0] = "This";
+            array[1] = "is";
+            array[2] = "an";
+            array[3] = "array";
+            array[4] = "of";
+            array[5] = "string";
+            return array;
+        }
+    }
+}
