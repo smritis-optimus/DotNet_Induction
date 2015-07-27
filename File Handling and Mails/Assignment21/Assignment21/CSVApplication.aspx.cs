b26e51728eda2cf17103c87e07fa468abb2a6d0f
Smriti Singh <smriti.singh@optimusinfo.com>
2015-07-27 16:02:46 +0530

    Completed Assignment 21

---
+++ File Handling and Mails/Assignment21/Assignment21/CSVApplication.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Assignment21
+{
+    public partial class CSVApplication : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSaveToDB_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (fileUploadCSV.HasFiles)
+                {
+                    //save the file in a specified folder
+                   fileUploadCSV.SaveAs(Server.MapPath("~/")+fileUploadCSV.FileName);
+                    //calling the function to read all contents from csv file
+                    if (UtilityClass.LoadFromCSV(Server.MapPath(fileUploadCSV.FileName)))
+                        lblMessage.Text="All data read from csv file and inserted";
+                    else
+                        lblMessage.Text="Some error occured";
+                }
+
+                else
+                {
+                    lblMessage.Text = "you have not spcefied a file";
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Error occured :"+ ex.Message);
+            }
+        }
+    }
+}
---
+++ File Handling and Mails/Assignment21/Assignment21/StudentDataHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Assignment21
+{
+    public class StudentDataHandler
+    {
+
+        public static bool insertStudents(List<Student> studentsData)
+        {
+            SqlConnection con;
+            SqlCommand cmd;
+            string connection = ConfigurationManager.ConnectionStrings["StudentDB"].ConnectionString;
+            try
+            {
+                //every record of the student list is taken and inserted in the database.
+                //Query is executed for insertion of every record
+                foreach (Student student in studentsData)
+                {
+                    string query = "insert into Student values('" + student.RollNo + "','" + student.Name + "','" + student.Gender + "','" + student.Age + "','" + student.Stream + "')";
+                    using (con = new SqlConnection(connection))
+                    {
+                        cmd = new SqlCommand(query, con);
+                        con.Open();
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
