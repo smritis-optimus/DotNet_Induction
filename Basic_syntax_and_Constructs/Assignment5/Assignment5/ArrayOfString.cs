bc7a4168477f0b488f646320bc5f2c5819d64fba
Smriti Singh <smriti.singh@optimusinfo.com>
2015-07-28 10:54:00 +0530

    Implemented the changes

---
+++ Basic_syntax_and_Constructs/Assignment5/Assignment5/ArrayOfString.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Assignment5
+{
+    class ArrayOfString
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(" ", GetArrayofString()));
+            Console.Read();
+        }
+        public static String[] GetArrayofString()
+        {
+            string[] array = new string[6];
+            array[0] = "This";
+            array[1] = "is";
+            array[2] = "an";
+            array[3] = "array";
+            array[4] = "of";
+            array[5] = "string";
+            return array;
+        }
+    }
+}
