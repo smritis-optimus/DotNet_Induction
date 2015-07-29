eae63c6ef57d82c5e08570417695a9b75d5acbd5
Smriti Singh <smriti.singh@optimusinfo.com>
2015-07-29 19:45:23 +0530

    Completed Assignment 29

---
+++ XML and Serialization/Assignment29/Assignment29/CreateXml.aspx.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Xml;
+namespace Assignment29
+{
+    public partial class CreateXml : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string path = Server.MapPath("~/Students.xml");
+            XmlTextWriter textWriter = new XmlTextWriter(path, null);
+            textWriter.WriteStartDocument();
+            textWriter.WriteStartElement("Students");
+            textWriter.WriteStartElement("Student");
+            textWriter.WriteAttributeString("name", "Preeti");
+            textWriter.WriteAttributeString("age", "22");
+            textWriter.WriteStartElement("Stream");
+            textWriter.WriteString("PCM");
+            textWriter.WriteEndElement();
+            textWriter.WriteStartElement("Address");
+            textWriter.WriteStartElement("City");
+            textWriter.WriteString("Delhi");
+            textWriter.WriteEndElement();
+            textWriter.WriteStartElement("Country");
+            textWriter.WriteString("India");
+            textWriter.WriteEndElement();
+            textWriter.WriteEndElement();
+            textWriter.WriteEndElement();
+            textWriter.WriteStartElement("Student");
+            textWriter.WriteAttributeString("name", "Priya");
+            textWriter.WriteAttributeString("age", "24");
+            textWriter.WriteStartElement("Stream");
+            textWriter.WriteString("PCB");
+            textWriter.WriteEndElement();
+            textWriter.WriteStartElement("Address");
+            textWriter.WriteStartElement("City");
+            textWriter.WriteString("Mumbai");
+            textWriter.WriteEndElement();
+            textWriter.WriteStartElement("Country");
+            textWriter.WriteString("India");
+            textWriter.WriteEndElement();
+            textWriter.WriteEndElement();
+            textWriter.WriteEndElement();
+            textWriter.WriteStartElement("Student");
+            textWriter.WriteAttributeString("name", "Anjali");
+            textWriter.WriteAttributeString("age", "25");
+            textWriter.WriteStartElement("Stream");
+            textWriter.WriteString("Commerce");
+            textWriter.WriteEndElement();
+            textWriter.WriteStartElement("Address");
+            textWriter.WriteStartElement("City");
+            textWriter.WriteString("Delhi");
+            textWriter.WriteEndElement();
+            textWriter.WriteStartElement("Country");
+            textWriter.WriteString("India");
+            textWriter.WriteEndElement();
+            textWriter.WriteEndElement();
+            textWriter.WriteEndElement();
+            textWriter.WriteStartElement("Student");
+            textWriter.WriteAttributeString("name", "Mohan");
+            textWriter.WriteAttributeString("age", "24");
+            textWriter.WriteStartElement("Stream");
+            textWriter.WriteString("PCM");
+            textWriter.WriteEndElement();
+            textWriter.WriteStartElement("Address");
+            textWriter.WriteStartElement("City");
+            textWriter.WriteString("Chennai");
+            textWriter.WriteEndElement();
+            textWriter.WriteStartElement("Country");
+            textWriter.WriteString("India");
+            textWriter.WriteEndElement();
+            textWriter.WriteEndElement();
+            textWriter.WriteEndElement();
+            textWriter.WriteStartElement("Student");
+            textWriter.WriteAttributeString("name", "Shivam");
+            textWriter.WriteAttributeString("age", "22");
+            textWriter.WriteStartElement("Stream");
+            textWriter.WriteString("PCM");
+            textWriter.WriteEndElement();
+            textWriter.WriteStartElement("Address");
+            textWriter.WriteStartElement("City");
+            textWriter.WriteString("Delhi");
+            textWriter.WriteEndElement();
+            textWriter.WriteStartElement("Country");
+            textWriter.WriteString("India");
+            textWriter.WriteEndElement();
+            textWriter.WriteEndElement();
+            textWriter.WriteEndElement();
+            textWriter.WriteEndElement();
+            textWriter.WriteEndDocument();
+            textWriter.Close();
+
+        }
+    }
+}
---
+++ XML and Serialization/Assignment29/Assignment29/viewXml.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Xml;
+using System.Web;
+using System.Xml.Xsl;
+using System.Xml.XPath;
+namespace Assignment29
+{
+    public partial class viewXml : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string filename = Server.MapPath(@"~/Students.xml");
+            string stylesheet = Server.MapPath(@"~/Student.xslt");
+            XmlDocument document = new XmlDocument();
+            //load the xml file
+            document.Load(Server.MapPath(@"~\Students.xml"));
+            XmlNode root = document.DocumentElement;
+            foreach (XmlNode node in root.ChildNodes)
+            {
+                //if stream is PCM add a new node of college
+                if (node.ChildNodes.Item(0).InnerText.Equals("PCM"))
+                {
+                    XmlElement newelement = document.CreateElement("College");
+                    newelement.InnerText = "Engineering";
+                    node.AppendChild(newelement);
+                }
+            }
+            document.Save(Server.MapPath(@"~\Students.xml"));
+            //create an object of XPathDocument
+            XPathDocument myXPathDoc = new XPathDocument(filename);
+            //Load the transformation from the stylesheet
+            XslCompiledTransform myXslTrans = new XslCompiledTransform();
+            myXslTrans.Load(stylesheet);
+            //load it into result.html to show the result
+            XmlTextWriter myWriter = new XmlTextWriter(Server.MapPath(@"~/result.html"), null);
+            myXslTrans.Transform(myXPathDoc, null, myWriter);
+            Response.Redirect("result.html");
+        }
+    }
+}
